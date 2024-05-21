65e16bcd077d46520a08ef4a4908e219f2080583
robert <>
2024-05-20 21:51:42 -0400

    separate utf8 parser

--- charset_parse.c
+++ charset_parse.c
@@ -6,25 +6,94 @@
  *escape sequences
  *
  */
-enum charset_fsm_cond_e
+enum utf8_codepoint_fsm_state_e
+{
+    CS_FSM_UTF8_FAIL = 0,
+    CS_FSM_UTF8_VALID_FIRST_BYTE,
+    CS_FSM_UTF8_CHAR_TAIL,
+    CS_FSM_UTF8_SUCCESS,
+    CS_FSM_UTF8_DECODE,
+    CS_FSM_ASCII_CHAR,
+    CS_FSM_UTF8_CHAR,
+    CS_FSM_UTF8_CHECK_BYTE_ONE,
+    CS_FSM_UTF8_VALID_TAIL_BYTE,
+
+};
+
+enum charset_fsm_state_e
 {
     CS_FSM_FAIL = 0,
     CS_FSM_SUCCESS,
-    CS_FSM_ASCII_CHAR_NOT_BSLASH_RBRACKET,
-    CS_FSM_RANGE,
-    CS_FSM_BYTES_LEFT,
-    CS_FSM_NO_TERMINATOR,
+    CS_FSM_CHECK_BSLASH,
+    CS_FSM_CP_DECODE,
+    CS_HANDLE_BSLASH,
+    CS_HANDLE_NOTSLASH,
     CS_FSM_STR_NONNULL,
-    CS_FSM_LBRACKET,
-    CS_FSM_ASCII_CHAR,
-    CS_FSM_UTF8_CHAR,
-    CS_FSM_BSLASH,
-    CS_FSM_ASCII_CHAR_NOT_BSLASH,
-    CS_FSM_UTF8_BYTES_LEFT,
-    CS_FSM_UTF8_CODEPOINT,
-    CS_FSM_UTF8_VALID_BYTE
+    CS_FSM_EXPECT_LBRACKET,
+    CS_FSM_CHECK_NEGATE,
+    CS_FSM_BYTES_LEFT,
+    CS_FSM_BODY,
+    CS_FSM_NEGATED
 };
-typedef enum charset_fsm_cond_e charset_fsm_cond_t;
+typedef enum charset_fsm_state_e charset_fsm_state_t;
+typedef enum utf8_codepoint_fsm_state_e utf8_codepoint_fsm_state_t;
+
+
+uint32_t 
+parse_utf8_codepoint(
+    const unsigned char * const restrict str,
+    uint32_t * const restrict cp
+);
+uint32_t 
+parse_utf8_codepoint(
+    const unsigned char * const restrict str,
+    uint32_t * const restrict cp
+)
+{
+    utf8_codepoint_fsm_state_t state;
+    uint32_t l = 0, i, j;
+    state = (!!str) * CS_FSM_UTF8_DECODE;
+top:
+    switch (state)
+    {
+    case CS_FSM_UTF8_FAIL:
+        return 0;
+    case CS_FSM_UTF8_VALID_FIRST_BYTE:
+        *cp |=((uint32_t)(((str[l] << i) & 0xFF)>> i)) << ((i-1)*6);
+        j = 1;
+        l++;
+    /*FALLTHROUGH*/
+    case CS_FSM_UTF8_CHAR_TAIL:
+        state = (str[l] >> 6 == 2) * CS_FSM_UTF8_VALID_TAIL_BYTE;
+        goto top;
+    case CS_FSM_UTF8_SUCCESS:
+        return l;
+    case CS_FSM_UTF8_DECODE:
+        state = (str[l] >> 7) + CS_FSM_ASCII_CHAR; /* OR CS_FSM_UTF8_CHAR */
+        i = 1;
+        j = 0;
+        *cp = 0;
+        goto top;
+    case CS_FSM_ASCII_CHAR:
+        *cp = str[l];
+        l++;
+        return l;
+    case CS_FSM_UTF8_CHAR:
+        i += (((str[l] << i) & 0xFF)>> 7);
+        state = (!(((str[l] << i) & 0xFF)>> 7)) + CS_FSM_UTF8_CHAR;
+        goto top;
+    case CS_FSM_UTF8_CHECK_BYTE_ONE:
+        state = (i > 1 && i < 5) * CS_FSM_UTF8_VALID_FIRST_BYTE;
+        goto top;
+    case CS_FSM_UTF8_VALID_TAIL_BYTE:
+        *cp |=(uint32_t)(str[l] & 0x3F) << ((i-1-j) * 6);
+        j++;
+        l++;
+        state = (j>=i) + CS_FSM_UTF8_CHAR_TAIL;
+        goto top;
+    }
+}
+
 
 uint32_t
 parse_charset(
@@ -35,63 +104,54 @@ uint32_t
 parse_charset(
     const unsigned char * const restrict str
 ){
-    uint32_t i, j, l = 0;
-    charset_fsm_cond_t condition;
-    condition = (!!str) * CS_FSM_STR_NONNULL;
-
+    uint32_t i, cp, l = 0;
+    charset_fsm_state_t state, cp_exit;
+    state = (!!str) * CS_FSM_STR_NONNULL;
+    uint8_t negated;
 top:
-    switch (condition)
+    switch (state)
     {
     case CS_FSM_FAIL:
         return 0;
     case CS_FSM_SUCCESS:
         return l;
-    case CS_FSM_ASCII_CHAR_NOT_BSLASH_RBRACKET:
-        condition = !(str[l+1] == '-');
-        l += condition;
-        condition += CS_FSM_RANGE;
+    case CS_FSM_CHECK_BSLASH:
+        state = ( cp != '\\') + CS_HANDLE_BSLASH;
         goto top;
-    case CS_FSM_RANGE_PREFIX:
-        condition = (str[l] <= str[l+2] && (str[l+2] >> 7 == 0)) * CS_FSM_BYTES_LEFT;
-        l+=3;
-        goto top;
-    case CS_FSM_BYTES_LEFT:
-        condition = (!!str[l]) * CS_FSM_NO_TERMINATOR;
-        goto top;
-    case CS_FSM_NO_TERMINATOR:
-        condition = (str[l] >> 7) + CS_FSM_ASCII_CHAR;
+    case CS_FSM_CP_DECODE:
+        i = parse_utf8_codepoint(str + l, &cp);
+        l+=i;
+        state = (!!i) * cp_exit;
         goto top;
     case CS_FSM_STR_NONNULL:
-        condition = (str[l] == '[') * CS_FSM_LBRACKET;
-        l++;
-        goto top;    
-    case CS_FSM_LBRACKET:
-        l += (str[l] == '^');
-        condition = CS_FSM_BYTES_LEFT;
+        state = CS_FSM_CP_DECODE;
+        cp_exit = CS_FSM_EXPECT_LBRACKET;
         goto top;
-    case CS_FSM_ASCII_CHAR:
-        condition = (str[l] != '\\') + CS_FSM_BSLASH;
+    case CS_HANDLE_BSLASH:
+        /*TODO: Not implemented*/
+        state=CS_FSM_FAIL;
         goto top;
-    case CS_FSM_UTF8_CHAR:
-        for (i = 0; (str[l] << (i + 1)) & (1 << 7); i++);
-        condition =  (i!=0) * CS_FSM_UTF8_BYTES_LEFT;
-        l++;
+    case CS_HANDLE_NOTSLASH:
+        /*TODO: Not implemented*/
+        state=CS_FSM_FAIL;
         goto top;
-    case CS_FSM_BSLASH:
-        l++;
-    case CS_FSM_ASCII_CHAR_NOT_BSLASH:
-        condition = (str[l] != ']') + CS_FSM_SUCCESS;
+    case CS_FSM_EXPECT_LBRACKET:
+        state = (cp == '[') * CS_FSM_CP_DECODE;
+        cp_exit = CS_FSM_CHECK_NEGATE; 
         goto top;
-    case CS_FSM_UTF8_BYTES_LEFT:
-        condition = (((3 << 6) & str[l]) == (1<<7)) * CS_FSM_UTF8_VALID_BYTE;
-        l++;
+    case CS_FSM_CHECK_NEGATE:
+        negated = cp=='^';
+        state = negated + CS_FSM_BODY;
         goto top;
-    case CS_FSM_UTF8_CODEPOINT:
-        condition = CS_FSM_BYTES_LEFT;
+    case CS_FSM_BYTES_LEFT:
+        state = (cp != ']') + CS_FSM_SUCCESS;
         goto top;
-    case CS_FSM_UTF8_VALID_BYTE:
-        i--;
-        condition = (i < 1) + CS_FSM_UTF8_BYTES_LEFT;
+    case CS_FSM_BODY:
+        state = (cp != 0) * CS_FSM_BYTES_LEFT;
+        goto top;
+    case CS_FSM_NEGATED:
+        cp_exit = CS_FSM_BODY;
+        state = CS_FSM_CP_DECODE;
         goto top;
     }
 }
