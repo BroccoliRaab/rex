e171193dfdf17ef2fbadbd25a040d2856b025631
robert <>
2025-09-03 19:08:06 -0400

    Fixed infinite loop

--- test.c
+++ test.c
@@ -114,7 +114,6 @@ static inline void rex_mcset_body_simplify(
     uint32_t * const io_r1
 )
 {
-    size_t l, i;
     uint32_t cp0, cp1;
     uint32_t min_cp0, min_cp1;
 
@@ -133,62 +132,23 @@ static inline void rex_mcset_body_simplify(
             &cp0,
             &cp1
         );
-        if (cp0 > min_cp1+1) break;
+        if (cp0 > REX_MIN(min_cp1+1, REX_MAX_UNICODE_VAL)) break;
         /* Merge ranges */
         min_cp1 = cp1;
     };
     *io_r0 = min_cp0;
     *io_r1 = min_cp1;
 }
-  
 
-int main(int argc, char ** argv)
+void print_ranges(
+    char * re_str
+)
 {
-    rex_compiler_t rexc;
-    uint8_t mem[1024];
-    unsigned char re_str[512];
-    size_t re_str_sz;
-    size_t i;
-    rex_instruction_t prog[128];
-    int r;
-    uint32_t cp0, cp1;
-
-    rexc.memory = mem;
-    rexc.memory_sz = 1024;
-    for (;;)
-    {
-    if (!fgets(re_str, 512, stdin))
-    {
-        continue;
-    }
-    
-    re_str_sz = strlen(re_str);
-    re_str[re_str_sz--] = 0;
-
-    i = rex_parse_charset(
-        re_str,
-        re_str_sz+1
-    );
-
-    if (i == 0)
-    {
-        puts("Parse Failure");
-        continue;
-    } else if (i == re_str_sz)
-    {
-        puts("Parse Sucess");
-    }else{
-        puts("Trailing Characters");
-        continue;
-    }
-    if (re_str[0] != '[') continue;
-    if (re_str[1] == '^') continue;
-    puts("As sorted Ranges:");
-    
+    uint32_t cp0, cp1;    
     cp0 = 0, cp1 = REX_MAX_UNICODE_VAL;
     for(;;) {
         rex_mcset_body_simplify(
-            re_str+1,
+            re_str,
             &cp0,
             &cp1
         );
@@ -198,6 +158,77 @@ int main(int argc, char ** argv)
         cp1 = REX_MAX_UNICODE_VAL;
     };
     puts("\n");
+}
+
+void print_ranges_inverted(
+    char * re_str
+)
+{
+    uint32_t cp0, cp1;
+    uint32_t inv0, inv1;
+    inv0 = 0;
+    inv1 = 0;
+    cp0 = 0, cp1 = REX_MAX_UNICODE_VAL;
+    for(;;) {
+        rex_mcset_body_simplify(
+            re_str,
+            &cp0,
+            &cp1
+        );
+        if (cp0 > REX_MAX_UNICODE_VAL) break;
+        printf("[%c, %c], ", cp0, cp1);
+        cp0 = cp1 + 1;
+        cp1 = REX_MAX_UNICODE_VAL;
+    };
+    puts("\n");
+}
+
+
+int main(int argc, char ** argv)
+{
+    rex_compiler_t rexc;
+    uint8_t mem[1024];
+    char re_str[512];
+    size_t re_str_sz;
+    size_t i;
+
+    rexc.memory = mem;
+    rexc.memory_sz = 1024;
+    for (;;)
+    {
+        if (!fgets(re_str, 512, stdin))
+        {
+            continue;
+        }
+        
+        re_str_sz = strlen(re_str);
+        re_str[re_str_sz--] = 0;
+
+        i = rex_parse_charset(
+            re_str,
+            re_str_sz+1
+        );
+
+        if (i == 0)
+        {
+            puts("Parse Failure");
+            continue;
+        } else if (i == re_str_sz)
+        {
+            puts("Parse Sucess");
+        }else{
+            puts("Trailing Characters");
+            continue;
+        }
+        if (re_str[0] != '[') continue;
+        puts("As sorted Ranges:");
+        if (re_str[1] == '^')
+        {
+        }else
+        {
+            print_ranges(re_str+1);
+        }
+
     }
     return 0;
 }
